32c8f42e284ab3e450c2ae4c8ab1b4c8b5a2fcac
Ayo Michaels <90911968+ayomichaels@users.noreply.github.com>
2023-08-16 13:46:24 +0100

    stuck at file upload

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import './PostShare.css'
 import ProfileImage from '../../img/profileImg.jpg'
 
@@ -6,8 +6,21 @@ import { UilScenery } from '@iconscout/react-unicons'
 import { UilPlayCircle } from '@iconscout/react-unicons'
 import { UilLocationPoint } from '@iconscout/react-unicons'
 import { UilSchedule } from '@iconscout/react-unicons'
+import { UilTimes} from '@iconscout/react-unicons'
+
 
 const PostShare = () => {
+  const [image, setImage] = useState(null)
+  const imageRef = useRef()
+
+  const onImageChange=(e)=>{
+    if(e.target.target.files && e.target.files[0]) {
+        let img = e.target.files[0];
+        setImage({
+            image: URL.createObjectURL(img)
+        })
+    }
+  }
   return (
     <div className="PostShare">
         <img src={ProfileImage} alt="" />
@@ -15,10 +28,11 @@ const PostShare = () => {
             <input type="text" name="" id="" placeholder="What's happening" />
             <div className="postOptions">
                 <div className="option" 
-                style={{color : "var(--photo)"}}>
+                style={{color : "var(--photo)"}} onClick={()=>imageRef.current.click()}>
                     <UilScenery/>
                     Photo
                 </div>
+
                 <div className="option"
                 style={{color : "var(--video)"}}>
                     <UilPlayCircle/>
@@ -37,7 +51,23 @@ const PostShare = () => {
                 <button className="button ps-button">
                     Share
                 </button>
+                <div style= {{display:'none'}}>
+                    <input 
+                        type="file" 
+                        name='myImage' 
+                        ref={imageRef} 
+                        onChange={onImageChange} 
+                    />
+                </div>
             </div>
+
+        {image && (
+            <div className="previewImage">
+                <UilTimes />
+                <img src={image.image} alt="" />
+            </div>
+        )}
+
         </div>
     </div>
   )
